4478fb1f9d9e94cf3e4609e02f39471af1e42010
DiegoZM1006 <1109184718@u.icesi.edu.co>
2025-09-10 23:15:07 -0500

    feat: update db

--- db.py
+++ db.py
@@ -1,20 +1,93 @@
-from sqlalchemy import create_engine, Column, Integer, String, JSON, Table, MetaData
+from sqlalchemy import create_engine, Column, Integer, String, JSON, Table, MetaData, Float, DateTime, Text, Boolean, ForeignKey, Date
 import databases
+from datetime import datetime
+from sqlalchemy.dialects.postgresql import UUID
+import uuid
 
 DATABASE_URL = "postgresql+asyncpg://tug_user:tug_pass@db:5432/tug_db"
+SYNC_DATABASE_URL = "postgresql://tug_user:tug_pass@db:5432/tug_db"
 
 database = databases.Database(DATABASE_URL)
 metadata = MetaData()
 
-resultados = Table(
-    "resultados",
+patient = Table(
+    "patient",
     metadata,
-    Column("id", Integer, primary_key=True, autoincrement=True),
-    Column("nombre_archivo", String),  
-    Column("muestra_id", Integer),
-    Column("timestamp", String),       
-    Column("resultado", JSON)          
+    Column("id", String, primary_key=True, default=uuid.uuid4),
+    Column("national_id", String)  
 )
 
-engine = create_engine(DATABASE_URL.replace("+asyncpg", ""))
-metadata.create_all(engine)
+test = Table(
+    "test",
+    metadata,
+    Column("id", String, primary_key=True, default=uuid.uuid4),
+    Column("file_name", String),
+    Column("patient_id", String, ForeignKey("patient.id"))
+)
+
+sample = Table(
+    "sample",
+    metadata,
+    Column("id", String, primary_key=True, default=uuid.uuid4),
+    Column("number", String),  
+    Column("date", Date),      
+    Column("test_id", String, ForeignKey("test.id"))
+)
+
+sample_graphics = Table(
+    "sample_graphics",
+    metadata,
+    Column("graphic_id", String,ForeignKey("graphics.id")),
+    Column("sample_id", String, ForeignKey("sample.id"))
+)
+
+graphics = Table(
+    "graphics",
+    metadata,
+    Column("id", String, primary_key=True, default=uuid.uuid4),
+    Column("name", String)
+)
+
+metricvalue = Table(
+    "metricvalue",
+    metadata,
+    Column("id", String, primary_key=True, default=uuid.uuid4), 
+    Column("value", String),
+    Column("metric_id", String, ForeignKey("metric.id")),
+    Column("sample_id", String, ForeignKey("sample.id"))
+)
+
+metric = Table(
+    "metric",
+    metadata,
+    Column("id", String, primary_key=True, default=uuid.uuid4),
+    Column("name", String),
+    Column("is_active", Boolean, default=True),
+    Column("phase_id", String, ForeignKey("phase.id")),
+    Column("measurement_units_id", String, ForeignKey("measurement_units.id"))
+)
+
+phase = Table(
+    "phase",
+    metadata,
+    Column("id", String, primary_key=True, default=uuid.uuid4),
+    Column("name", String)
+)
+
+measurement_units = Table(
+    "measurement_units",
+    metadata,
+    Column("id", String, primary_key=True, default=uuid.uuid4),
+    Column("name", String)
+)
+
+def init_db():
+    engine = create_engine(SYNC_DATABASE_URL)
+    try:
+        metadata.create_all(engine, checkfirst=True)
+        print("Tablas creadas o verificadas!")
+    except Exception as e:
+        print(f"Error al crear tablas: {e}")
+        # Continuar si las tablas ya existen
+
+init_db()
